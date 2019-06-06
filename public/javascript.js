d40d7c8721ed249da2afc62773238322f6c2afda
Kanaihya Kumar <43424058+Kanaihya@users.noreply.github.com>
2019-06-06 14:36:56 +0530

    Update javascript.js

--- public/javascript.js
+++ public/javascript.js
@@ -19,7 +19,7 @@ var socket = io();
 // The user count. Can change when someone joins/leaves
 socket.on('count', function (data) {
   $('.user-count').html(data);
-  $('.chat').append('<p style="text-align:center;"><emp>' + user + ' is online.' + '</emp></p>');
+  $('.chat').append('<p style="text-align:center;"><emp>' + cookie.get('user') + ' is online.' + '</emp></p>');
 });
 
 // When we receive a message
